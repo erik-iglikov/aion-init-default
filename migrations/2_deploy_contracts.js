4ea9e0dbd52e39fe024be3274cb04f81d06067e6
Sawyer <sawyercoletang@gmail.com>
2018-08-10 12:41:54 -0400

    change to Migrations.sol construstor and add HelloWorld.so

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var HelloWorld = artifacts.require("./HelloWorld.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(HelloWorld);
 };
