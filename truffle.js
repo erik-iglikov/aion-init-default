c6d7c5c51cb33e01f0bcc4c9698e412c8900811d
Sawyer <sawyercoletang@gmail.com>
2018-08-13 12:28:10 -0400

    fixed truffle.js config file

--- truffle.js
+++ truffle.js
@@ -1,18 +1,18 @@
 module.exports = {
   networks: {
     external: {
-      host: "0.0.0.0",    // external's node ip
+      host: "13.66.210.204",
       port: 8545,
-      network_id: "*",    // Match any network id
-      from: "",           // default account e.g. 0x000000000
-      accountPassword: "" // default password
+      network_id: "128", // Match any network id
+      from: "0xa04be405fe794146df4a6a0cac0009933323d65e29dedfaf80a1f880fa8cd329", // default account
+      accountPassword: "PLAT4life" // default password
     },
     development: {
-      host: "localhost",
+      host: "127.0.0.1",
       port: 8545,
-      network_id: "*",    // Match any network id
-      from: "",           // default account e.g. 0x000000000
-      accountPassword: "" // default password
+      network_id: "128", // Match any network id
+      from: "0xa04be405fe794146df4a6a0cac0009933323d65e29dedfaf80a1f880fa8cd329", // default account
+      accountPassword: "PLAT4life" // default password
     }
   }
 };
