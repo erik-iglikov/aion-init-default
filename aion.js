9b97db15d05ece12f072631f1d783ce6f4272888
Erik Iglikov <31218827+erik-iglikov@users.noreply.github.com>
2018-08-16 13:48:10 -0400

    Update aion.js

--- aion.js
+++ aion.js
@@ -3,7 +3,7 @@ module.exports = {
     external: {
       host: "13.66.210.204",
       port: 8545,
-      network_id: "128", // Match any network id
+      network_id: "256", // Match external network id
       from: "0xa04be405fe794146df4a6a0cac0009933323d65e29dedfaf80a1f880fa8cd329", // default account
       accountPassword: "PLAT4life" // default password
     },
